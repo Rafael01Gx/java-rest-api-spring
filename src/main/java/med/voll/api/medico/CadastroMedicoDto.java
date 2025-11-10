7c21df65b9f7abf44cf8e22ea4588fb909085388
rafael01gx <rafael_junio_moraes@hotmail.com>
2025-11-09 21:53:23 -0300

    feat: Implementa Bean Validation

--- src/main/java/med/voll/api/medico/CadastroMedicoDto.java
+++ src/main/java/med/voll/api/medico/CadastroMedicoDto.java
@@ -1,6 +1,26 @@
 package med.voll.api.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.EnderecoDto;
 
-public record CadastroMedicoDto(String nome, String email, String crm, Especialidade especialidade, EnderecoDto endereco) {
+public record CadastroMedicoDto(
+        @NotNull
+        @NotBlank
+        String nome,
+        @NotNull
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")
+        String crm,
+        @NotNull
+        Especialidade especialidade,
+        @NotNull
+        @Valid
+        EnderecoDto endereco) {
 }
