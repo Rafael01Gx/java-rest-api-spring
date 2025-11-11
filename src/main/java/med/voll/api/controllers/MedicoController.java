efe83b97b2e373e59b160139a12c42d731dcabc4
rafael01gx <rafael_junio_moraes@hotmail.com>
2025-11-11 19:56:37 -0300

    config: Configura nomes de  QueryParams

--- src/main/java/med/voll/api/controllers/MedicoController.java
+++ src/main/java/med/voll/api/controllers/MedicoController.java
@@ -9,6 +9,7 @@ import med.voll.api.services.MedicoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,7 +30,7 @@ public class MedicoController {
 
 
     @GetMapping
-    public ResponseEntity<Page<ListagemMedicoDto>> listar(Pageable paginacao){
+    public ResponseEntity<Page<ListagemMedicoDto>> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
         return ResponseEntity.ok(medicoService.listar(paginacao));
     }
 }
