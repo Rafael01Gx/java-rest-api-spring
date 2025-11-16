90106b1a5ab6eb4d779a1c14fd05941f164d9ec7
rafael01gx <rafael_junio_moraes@hotmail.com>
2025-11-15 23:16:43 -0300

    feat: adiciona e configura Documentação de API com SpringDoc "SWAGGER".

--- src/main/java/med/voll/api/controllers/MedicoController.java
+++ src/main/java/med/voll/api/controllers/MedicoController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controllers;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import med.voll.api.domain.medico.AtualizarMedicoDto;
 import med.voll.api.domain.medico.CadastroMedicoDto;
@@ -16,6 +17,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/medicos")
+@SecurityRequirement(name = "bearer-key")
 public class MedicoController {
 
     @Autowired
